6a79ed9e2c9bf339af7546a9a62a664d2daa5a8f
Tuffy14 <saleemabagwan232@gmail.com>
2025-12-06 20:43:27 +0530

    Redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/ChoZinWin/',
+  base: '/ChoZinWin/',   // 👈 IMPORTANT
   build: {
-    outDir: 'docs'   // 👈 THIS IS THE FIX
-  }
+    outDir: 'dist',      // build goes to /dist
+  },
 })
